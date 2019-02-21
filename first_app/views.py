00dd331b16ff9b0665d205be3b57f524efa4af5c
User <user@user-V520-15IKL>
2019-02-21 17:39:53 +0530

    21Feb-2

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
 # Create your views here.
 
 def home(request) :
-    return HttpResponse("<h1>Hello HOME</h1>")
+    my_dict = {"var" : "Template"}
+    return render(request , 'temp.html' , my_dict)
