54296e429a083ba8262009767642d9d9949325be
User <user@user-V520-15IKL>
2019-02-22 17:16:25 +0530

    22Feb-4

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from first_app.models import Name,Topic,WebPage
+
+admin.site.register(Name)
+admin.site.register(Topic)
+admin.site.register(WebPage)
--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
+#
 # Create your models here.
+class Topic(models.Model):
+    topic_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.topic_name
+
+
+class Name(models.Model):
+    topic_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.topic_name
+
+
+class WebPage(models.Model):
+    topic = models.ForeignKey(to=Topic, on_delete=None)
+
+    def __str__(self):
+        return self.topic
